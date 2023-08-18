2bc8081c8da702960396dbb379adf562a1deeec6
Eric Ford <eford@psu.edu>
2023-08-18 08:58:10 -0400

    maybe need to deploy docs from master

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, MyProject
 
 makedocs(modules = [MyProject], sitename = "MyProject.jl")
 
-deploydocs(repo = "github.com/PsuAstro528/project-template.git", devbranch="main")
+deploydocs(repo = "github.com/PsuAstro528/project-template.git") # , devbranch="main")
