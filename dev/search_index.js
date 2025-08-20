80e0860722470db37685c7dfd0c93558393e0ccf
Documenter.jl <documenter@juliadocs.github.io>
2025-08-20 21:18:19 +0000

    build based on e9af829

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyProject","page":"MyProject","title":"MyProject","text":"","category":"section"},{"location":"","page":"MyProject","title":"MyProject","text":"Example Julia package repo.","category":"page"},{"location":"#MyProject.addfive-Tuple{Number}","page":"MyProject","title":"MyProject.addfive","text":"addfive(x::Number)\n\nReturn x + 5.\n\n\n\n\n\n","category":"method"}]
+}
