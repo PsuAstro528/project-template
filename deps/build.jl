099816095fce892a4d99a05dcf6173b2b264027d
Eric Ford <eford@psu.edu>
2025-08-22 06:54:12 -0400

    update project

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,12 @@
+# Uncomment and adapt if your project uses Pluto notebooks
+#println("Making sure Pluto is installed")
+#import Pkg; 
+#import Pluto; 
+#
+#println("Installing packages for ex1.jl")
+#Pluto.activate_notebook_environment("../ex1.jl"); 
+#Pkg.instantiate(); 
+
+
 # julia-actions/julia-buildpkg
 touch(joinpath(homedir(), "julia-buildpkg"))
-
